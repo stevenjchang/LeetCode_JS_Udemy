64c653c459966807947229a42778575e9391c183
stevenjchang <stevenjchang@gmail.com>
2019-08-08 14:33:12 -0700

    LC3_lengthOfLongestSubstring passing

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,25 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+function lengthOfLongestSubstring(s) {
+  let hash = {};
+  let subString = "";
+  let longestSubstring = ""
+
+  for (let i = 0; i < s.length; i++) {
+    for (let j = 0; j < s.length - i; j++) {
+      let letter = s[i+j]
+      if (!hash[letter]) {
+        hash[letter] = 1;
+        subString += letter;
+      } else {
+        if (subString.length > longestSubstring.length) longestSubstring = subString;
+        hash = {};
+        subString = "";
+      }
+    }
+  }
+  return longestSubstring.length
+}
 
 module.exports = lengthOfLongestSubstring;
