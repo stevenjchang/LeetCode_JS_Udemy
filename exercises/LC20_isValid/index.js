d20d9a2e69c6137156421b3dd392a838a60bd425
stevenjchang <stevenjchang@gmail.com>
2019-08-10 14:19:28 -0700

    LC20

--- exercises/LC20_isValid/index.js
+++ exercises/LC20_isValid/index.js
@@ -1,3 +1,19 @@
-const isValid = s => {};
+const isValid = s => {
+  let stack = [];
+  let pairs = {
+    '{': '}',
+    '[': ']',
+    '(': ')'
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+
+    if (pairs[char]) stack.push(char)
+    else if (pairs[stack.pop()] !== char) return false
+  }
+
+  return stack.length === 0;
+};
 
 module.exports = isValid;
