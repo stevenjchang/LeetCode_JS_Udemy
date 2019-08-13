0166c8c7300725d16c6b3c615c9ca5e3156b68cf
stevenjchang <stevenjchang@gmail.com>
2019-08-13 15:45:35 -0700

    LC49_groupAnagrams

--- exercises/LC49_groupAnagrams/index.js
+++ exercises/LC49_groupAnagrams/index.js
@@ -1,3 +1,13 @@
-function groupAnagrams(strs) {}
+function groupAnagrams(strs) {
+  let result = {}
+
+  strs.forEach((str) => {
+    let alphabetizedString = str.split('').sort().join('')
+    if (result[alphabetizedString]) result[alphabetizedString].push(str)
+    else result[alphabetizedString] = [str]
+  })
+
+  return Object.values(result)
+}
 
 module.exports = groupAnagrams;
