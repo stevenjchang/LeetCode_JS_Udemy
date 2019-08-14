2d7d3b60d02a9526ecce83990120eb0a328e02c7
stevenjchang <stevenjchang@gmail.com>
2019-08-13 17:00:11 -0700

    LC123_isPalindrome

--- exercises/LC125_isPalindrome/index.js
+++ exercises/LC125_isPalindrome/index.js
@@ -1,3 +1,29 @@
-function isPalindrome(s) {}
+function isAlphaNumeric(char) {
+  let letterNumber = /^[0-9a-zA-Z]+$/;
+  return Boolean(char.match(letterNumber));
+}
+
+function isPalindrome(s) {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (!isAlphaNumeric(s[left])) {
+      left++;
+      continue;
+    }
+    if (!isAlphaNumeric(s[right])) {
+      right--;
+      continue;
+    }
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+      return false
+    } else {
+      left++;
+      right--
+    }
+  }
+  return true;
+}
 
 module.exports = isPalindrome;
